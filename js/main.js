8a044677f39db0ba36d8d876938633c4e7601c7b
AlexayStar <66807328+AlexayStar@users.noreply.github.com>
2022-06-12 20:56:14 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var deleteLog = false;
+
+$(document).ready(function () {
+  $('#pagepiling').pagepiling({
+    menu: '#myMenu',
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+    sectionsColor: ['#479ea4', '#479ea4', '#479ea4', '#479ea4'],
+
+    afterLoad: function (achortlink, index) {
+      if (index > 1) {
+        $('header').addClass('border');
+      } else {
+        $('header').removeClass('border');
+      }
+    }
+  });
+});
